e2291f91b8af758b48bb7e65989f6d14b8340580
Rushy <rushilshanmugam@gmail.com>
2026-03-27 12:25:16 -0400

    display lastname, age & gender on ID page

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,24 @@ router.route('/').get((req, res)=>{      //  /user
     //res.render
 }).post((req, res)=>{
     const firstName = req.body.firstName;
-    const isValid = firstName !=="";
+    const lastName = req.body.lastName;
+    const isValid = firstName !=="" && lastName !=="";
     if(isValid){
-        console.log(`Adding user: ${firstName}`)
-        users.push({firstName});
+        console.log(`Adding user: ${firstName},${lastName}`)
+        users.push({firstName},{lastName});
         res.render('users/list', {users});
     }
     else{
         console.log("Error adding user");
-        res.send('users/new', {firstName:firstName});
+        res.send('users/new', {firstName:firstName}, {lastName:lastName});
     }
 });
+
 router.get('/list',(req,res)=>{
     res.render('users/list', {users});
 
-})
+});
+
 router.get('/new', (req, res)=>{   //  /user/new
    res.render('users/new',{firstName:"Test"})
 });
@@ -31,7 +34,7 @@ router.get('/new', (req, res)=>{   //  /user/new
 router.route('/:id').get((req, res)=>{
     console.log(req.user);
     console.log('Getting user data!')
- res.send(`Getting User data for id: ${req.user['firstName']}`);
+ res.send(`User data: ${" "+req.user['firstName'] + "      " +req.user['lastName'] + "     " + req.user['age'] + "      " + req.user['gender']  }`);
 }).delete((req, res)=>{
 res.send(`Deleting User data for id: ${req.params.id}`);
 }).put((req, res)=>{
@@ -39,8 +42,8 @@ res.send(`Updating User data for id: ${req.params.id}`);
 });
 
 const users = [
-    {firstName: "Rushil"},
-     {firstName: "Bob"}
+    {firstName: "Rushil", lastName: "Shanmugam" , age:"22", gender:"Male"},
+     {firstName: "Bob", lastName: "Sagget" , age:"69", gender:"Male"}
 ];
 router.param("id", (req, res, next, id) =>{
     req.user = users[id];
